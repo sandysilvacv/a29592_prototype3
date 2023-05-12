6dad99ea8068afac2431eb93d568a3f31528cbb4
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-12 11:56:05 +0100

    3.Faça o jogador saltar no início
    usando o método AddForce para fazer o jogador pular no início do jogo

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Rigidbody playerRb;
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.AddForce(Vector3.up * 1000);
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
         
