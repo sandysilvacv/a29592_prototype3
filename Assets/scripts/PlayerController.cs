fa8e34500148ef81cea32dbbad16b2e94958e4ae
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-19 12:54:23 +0100

    4.Adicione um novo jogo sobre o gatilho
    Quando o jogador colide com um obstáculo, queremos acionar um estado de "Game Over" que pare tudo Para isso, precisamos de uma maneira de rotular e discernir todos os objetos do jogo com os quais o jogador colide.

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
+    public bool gameover = false;
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
@@ -26,6 +27,16 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+       
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameover = true;
+            Debug.Log("Game Over! ");
+        }
     }
 }
