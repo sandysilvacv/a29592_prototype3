993059ab5b050d6eb3cab8b66cfb73fc7b12fca9
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-19 12:26:13 +0100

    3.Fix repetição de fundo com colisor
    emos o fundo se repetindo a cada poucos segundos, mas a transição parece bastante estranha. Precisamos fazer o loop de fundo perfeitamente e perfeitamente com algumas novas variáveis.

--- Assets/scripts/RepeatBackground.cs
+++ Assets/scripts/RepeatBackground.cs
@@ -6,15 +6,17 @@ public class RepeatBackground : MonoBehaviour
 {
 
     private Vector3 startPos;
+    private float repeatWidth;
     void Start()
     {
         startPos = transform.position;
+        repeatWidth = GetComponent<BoxCollider>().size.x / 2;
     }
 
     
     void Update()
     {
-        if (transform.position.x < startPos.x -50)
+        if (transform.position.x < startPos.x - repeatWidth)
         {
             transform.position = startPos;
         }
