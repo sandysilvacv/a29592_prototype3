270a35c14d4906db444fb1a4862b4c9c2755c891
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-19 11:43:56 +0100

    9.Desova obstáculos em intervalos 86

--- Assets/scripts/SpawnManager.cs
+++ Assets/scripts/SpawnManager.cs
@@ -6,14 +6,24 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+
+    private float startDelay = 2;
+    private float repeatRate = 2;
+
+
     void Start()
     {
-        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
+        InvokeRepeating("SpawnObstacle", startDelay, repeatRate);
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void SpawnObstacle() 
+    {
+        Instantiate(obstaclePrefab, spawnPos, obstaclePrefab.transform.rotation);
     }
 }
