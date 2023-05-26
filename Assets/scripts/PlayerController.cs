74aa28755cf6d63a1edfe4590980d69bc405a1fa
sandysilvacv <127975123+sandysilvacv@users.noreply.github.com>
2023-05-26 12:22:01 +0100

    3.Adicione uma partícula de respingo de sujeira
    modifiquei os obstaculos substituindo o box collider para capsule collider porque no gameover meu player ficava no ar em vez do chão

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody playerRb;
     private Animator playerAnim;
     public ParticleSystem explosionParticle;
+    public ParticleSystem dirtParticle;
     public float jumpForce = 10;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -27,6 +28,7 @@ public class PlayerController : MonoBehaviour
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
             playerAnim.SetTrigger("Jump_trig");
+            dirtParticle.Stop();
         }
     }
 
@@ -36,6 +38,7 @@ public class PlayerController : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isOnGround = true;
+            dirtParticle.Play();
         }
 
         else if (collision.gameObject.CompareTag("Obstacle"))
@@ -45,6 +48,7 @@ public class PlayerController : MonoBehaviour
             playerAnim.SetBool ("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
             explosionParticle.Play();
+            dirtParticle.Stop();
         }
     }
 }
